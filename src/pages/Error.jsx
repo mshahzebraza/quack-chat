2b9d3fc5f0b8ae1d997aaabeb7093d57b7939c62
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2022-11-13 17:02:37 +0500

    Error Page UI complete

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+import "./Error.scss";
+
 const Error = () => {
   return (
-    <div>
-      Error Page
-      <Link to="/">Go Home</Link>;
-    </div>
+    <section>
+      <div className="mediaBox">
+        <h1>404</h1>
+        <div className="media" />
+      </div>
+      <div className="contentBox">
+        <h3 className="h2">Look like you're lost</h3>
+        <p>the page you are looking for not available!</p>
+        <Link className="toHome" to="/">
+          Go to Home
+        </Link>
+      </div>
+    </section>
   );
 };
 
