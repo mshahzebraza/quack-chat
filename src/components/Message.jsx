f0dfcac88d81aac247e4d6b5266c2b5362d70eb1
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2022-11-21 00:14:04 +0500

    add messaging functionality
    - still have to compare code and test all features thoroughly especially the messaging functionality and document structure

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,24 +1,51 @@
+import { useAtom } from "jotai";
+import { activeChatUserAtom } from "../../firebase/firestore";
+import { authUserAtom } from "../../firebase/auth";
 import "../styles/Message.scss";
 
 const Message = ({ message }) => {
-  const isOwner = Math.floor(Math.random() * 10) % 2 === 0; // generates a random boolean
-  //   const { currentUser } = useContext(AuthContext);
+  const {
+    img: imgPath,
+    text = "No Content",
+    uid: senderId,
+    date = {},
+  } = message;
+  const { seconds: messageTime } = date;
+  const [authUser, setAuthUser] = useAtom(authUserAtom);
+  const [activeChatUser, setActiveChatUser] = useAtom(activeChatUserAtom);
+  const senderImage = getSenderImage(senderId, authUser, activeChatUser);
+
+  // const isOwner = Math.floor(Math.random() * 10) % 2 === 0; // generates a random boolean
+  const isOwner = senderId === authUser.uid; // generates a random boolean
+  console.log("message time", messageTime);
 
   return (
     <div className={`message ${isOwner && "owner"}`}>
       <div className="messageInfo">
-        <img
-          src={"https://avatars.githubusercontent.com/u/65836206?s=40&v=4"}
-          alt="Sender's Image"
-        />
-        <span>just now</span>
+        <img src={senderImage} alt="Sender's Image" />
+        <span>{getMessageTime(messageTime)}</span>
       </div>
       <div className="messageContent">
-        <p>{message?.text || "No Content"}</p>
-        {message?.img && <img src={message?.img} alt="" />}
+        <p>{text}</p>
+        {imgPath && <img src={imgPath} alt="" />}
       </div>
     </div>
   );
 };
 
 export default Message;
+
+function getSenderImage(senderId, authUser, activeChatUser) {
+  const fallbackImage =
+    "https://avatars.githubusercontent.com/u/65836206?s=40&v=4";
+  if (senderId === authUser.uid) {
+    return authUser.photoURL;
+  } else if (senderId === activeChatUser.uid) {
+    return activeChatUser.photoURL;
+  } else return fallbackImage;
+}
+
+function getMessageTime(messageTime) {
+  const sentTime = new Date(messageTime).toLocaleDateString();
+  return sentTime;
+}
