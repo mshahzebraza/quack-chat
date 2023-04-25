e3ce6438bfc714ae9f32f9e0dc4c9fadfbc1ed93
M Shahzeb Raza <m.shahzebraza.97@gmail.com>
2023-04-25 18:28:34 +0500

    added logo

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -34,6 +34,9 @@ function BlankChat() {
 
   return (
     <div className="blankWindow">
+      {/* import CamImageURL from "../assets/cam.png"; */}
+
+      <img style={{ marginBottom: "1rem" }} src="/svgLogo.svg" alt="svg logo" />
       <h1>Welcome, {authUser.displayName} 👋</h1>
       <h2>Choose a Chat to start conversation!</h2>
     </div>
